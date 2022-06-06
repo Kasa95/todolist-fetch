8b212ddd1ee162d466f32bcee45747d802bd9063
Kasa95 <nachocasanova95@gmail.com>
2022-06-06 18:43:00 +0000

    v1

--- src/js/component/NewTask.jsx
+++ src/js/component/NewTask.jsx
@@ -1,22 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const NewTask = () => {
 	const [task, setTask] = useState("");
-	const [list, setList] = useState([
-		"Wake Up",
-		"Brush teeth",
-		"Take a Shower",
-	]);
-	const [tachado, setTachado] = useState("");
+	const [list, setList] = useState([]);
 	const [isShown, setIsShown] = useState(false);
 
 	function handleTask(event) {
 		setTask(event.target.value);
 	}
 
-	const addTask = () => {
-		setList([...list, task]);
+	const addTask = (task) => {
+		setList(list.push({ label: task, done: false }));
+
+		const requestOptionsInsertarTarea = {
+			method: "PUT",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(list),
+		};
+		fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/Kasa95",
+			requestOptionsInsertarTarea
+		).then((response) => response.json);
 	};
+
+	// const updateList = fetch(
+	// 	"https://assets.breatheco.de/apis/fake/todos/user/Kasa95",
+	// 	{
+	// 		method: "PUT",
+	// 		body: JSON.stringify(list),
+	// 		headers: {
+	// 			"Content-Type": "application/json",
+	// 		},
+	// 	}
+	// )
+	// 	.then((resp) => {
+	// 		console.log(resp.ok); // will be true if the response is successfull
+	// 		console.log(resp.status); // the status code = 200 or code = 400 etc.
+	// 		console.log(resp.text()); // will try return the exact result as string
+	// 		return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+	// 	})
+	// 	.then((data) => {
+	// 		//here is were your code should start after the fetch finishes
+	// 		console.log(data); //this will print on the console the exact object received from the server
+	// 	})
+	// 	.catch((error) => {
+	// 		//error handling
+	// 		console.log(error);
+	// 	});
 
 	function handleRemove(id) {
 		const newList = list.filter((item, index) => index !== id);
@@ -25,8 +55,8 @@ const NewTask = () => {
 	}
 
 	const listaTodo = list.map((item, index) => (
-		<div key={index} className={"row " + tachado}>
-			<div className="col-10">{item} </div>
+		<div key={index} className="row">
+			<div className="col-10">{item.label} </div>
 			<div className="col-2">
 				{isShown && (
 					<>
@@ -50,6 +80,16 @@ const NewTask = () => {
 		</div>
 	));
 	console.log(list);
+
+	useEffect(() => {
+		const getList = () => {
+			fetch("https://assets.breatheco.de/apis/fake/todos/user/Kasa95")
+				.then((response) => response.json())
+				.then((data) => setList(data));
+		};
+		getList();
+	}, []);
+
 	return (
 		<div className="container w-30 py-3 border mt-5">
 			<h1 className="text-center py-3 display-4">ToDo List</h1>
